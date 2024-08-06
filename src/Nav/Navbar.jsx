adb1aa44de71ef3258b2d176e49618337c745ecf
roshan <roshankr9894@gmail.com>
2024-08-06 10:33:23 +0530

    final

--- src/Nav/Navbar.jsx
+++ src/Nav/Navbar.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
-
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <a href="/">MyApp</a>
+        <a href="/">PortFolio</a>
       </div>
-      <ul className="navbar-links">
+      <button className="navbar-toggle" onClick={toggleNavbar}>
+        ☰
+      </button>
+      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
         <li><a href="#hero">Home</a></li>
-        <li><a href="#projects">projects</a></li>
+        <li><a href="#projects">Projects</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
